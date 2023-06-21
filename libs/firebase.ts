dd2ddb966abf10feaaade46d820b6be00150b6d5
david emioma <davidemiomauche@gmail.com>
2023-06-21 18:29:08 +0100

    Setup prisma and firebase storage

---
+++ libs/firebase.ts
@@ -0,0 +1,17 @@
+import { getStorage } from "firebase/storage";
+import { initializeApp, getApps, getApp } from "firebase/app";
+
+const app = !getApps().length
+  ? initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_APP_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_APP_AUTHDOMAIN,
+      projectId: process.env.NEXT_PUBLIC_APP_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_APP_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_APP_APP_ID,
+    })
+  : getApp();
+
+export const storage = getStorage();
+
+export default app;
