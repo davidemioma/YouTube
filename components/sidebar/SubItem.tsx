f9b27958bf5865774066685ad701d3c3d2f0e1c0
david emioma <davidemiomauche@gmail.com>
2023-06-26 19:20:28 +0100

    Fixed some issues in sidebar

--- components/sidebar/SubItem.tsx
+++ components/sidebar/SubItem.tsx
@@ -1,25 +1,35 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
 import Avatar from "../Avatar";
 import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import useSideModal from "@/hooks/useSideModal";
 
 interface Props {
   channel: User;
 }
 
 const SubItem = ({ channel }: Props) => {
-  return (
-    <Link href={`/channel/${channel.id}`}>
-      <div
-        className={`flex items-center gap-4 p-2.5 rounded-lg cursor-pointer hover:bg-[hsl(0,0%,18.82%)] transition`}
-      >
-        <Avatar imgSrc={channel.image!} small />
+  const router = useRouter();
 
-        <span className="text-sm">{channel.name}</span>
-      </div>
-    </Link>
+  const sideModal = useSideModal();
+
+  const onClickHandler = () => {
+    router.push(`/channel/${channel.id}`);
+
+    sideModal.onClose();
+  };
+
+  return (
+    <div
+      className={`flex items-center gap-4 p-2.5 rounded-lg cursor-pointer hover:bg-[hsl(0,0%,18.82%)] transition`}
+      onClick={onClickHandler}
+    >
+      <Avatar imgSrc={channel.image!} small />
+
+      <span className="text-sm">{channel.name}</span>
+    </div>
   );
 };
 
