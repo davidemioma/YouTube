3fceb95fb599d49c7c286ea70be0469d2b7e1dcc
david emioma <davidemiomauche@gmail.com>
2023-07-15 12:14:59 +0100

    Subscription page updated

--- app/subscriptions/components/SubsContent.tsx
+++ app/subscriptions/components/SubsContent.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "@/components/posts/Post";
 import { CurrentUser, PostProps } from "@/types";
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
+import useUnlimitedScrolling from "@/hooks/useUnlimitedScrolling";
+import Spinner from "@/components/Spinner";
 
 interface Props {
   currentUser: CurrentUser | null;
@@ -10,15 +13,42 @@ interface Props {
 }
 
 const SubsContent = ({ currentUser, initialPosts }: Props) => {
+  const { ref, entry, data, fetchNextPage, isFetchingNextPage } =
+    useUnlimitedScrolling({
+      key: "infinite-query-subscripton",
+      query: `/api/posts/subscription?limit=${INFINITE_SCROLL_PAGINATION_RESULTS}`,
+      initialData: initialPosts,
+    });
+
+  //@ts-ignore
+  const posts: PostProps[] =
+    data?.pages?.flatMap((page) => page) ?? initialPosts;
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
   return (
     <div className="w-full h-full p-5 overflow-y-auto">
       <span className="text-lg sm:text-xl font-semibold">Latest</span>
 
       <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 md:justify-items-start lg:grid-cols-3 2xl:grid-col-4 gap-5 py-5">
-        {initialPosts.map((post) => (
-          <Post key={post.id} post={post} currentUser={currentUser} />
-        ))}
+        {posts.map((post, i) => {
+          if (i === initialPosts.length - 1) {
+            return (
+              <div key={post.id} ref={ref}>
+                <Post post={post} currentUser={currentUser} />
+              </div>
+            );
+          } else {
+            return <Post key={post.id} post={post} currentUser={currentUser} />;
+          }
+        })}
       </div>
+
+      {isFetchingNextPage && <Spinner />}
     </div>
   );
 };
