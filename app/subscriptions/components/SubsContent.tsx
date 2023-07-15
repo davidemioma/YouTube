2a3f10f163a87a871355d4b205ecf2aa6d8bf8e3
david emioma <davidemiomauche@gmail.com>
2023-07-15 12:03:30 +0100

    Updated Homepage

--- app/subscriptions/components/SubsContent.tsx
+++ app/subscriptions/components/SubsContent.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import React from "react";
-import { CurrentUser, PostProps } from "@/types";
 import Post from "@/components/posts/Post";
+import { CurrentUser, PostProps } from "@/types";
 
 interface Props {
   currentUser: CurrentUser | null;
-  posts: PostProps[];
+  initialPosts: PostProps[];
 }
 
-const SubsContent = ({ currentUser, posts }: Props) => {
+const SubsContent = ({ currentUser, initialPosts }: Props) => {
   return (
     <div className="w-full h-full p-5 overflow-y-auto">
       <span className="text-lg sm:text-xl font-semibold">Latest</span>
 
       <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 md:justify-items-start lg:grid-cols-3 2xl:grid-col-4 gap-5 py-5">
-        {posts.map((post) => (
+        {initialPosts.map((post) => (
           <Post key={post.id} post={post} currentUser={currentUser} />
         ))}
       </div>
